420c030d521d04c19eb88889957c0e0d55408549
Manuel Rony Gomes <rgomes.bd@gmail.com>
2021-03-28 18:50:11 +0600

    Add BasicErrorPromptFormatter

---
+++ src/main/java/me/ronygomes/anoread/formatter/impl/BasicErrorPromptFormatter.java
@@ -0,0 +1,39 @@
+package me.ronygomes.anoread.formatter.impl;
+
+import me.ronygomes.anoread.exception.AnoReadException;
+import me.ronygomes.anoread.exception.ConversionException;
+import me.ronygomes.anoread.exception.ExtractionException;
+import me.ronygomes.anoread.exception.ValidationError;
+import me.ronygomes.anoread.formatter.ErrorPromptFormatter;
+import me.ronygomes.anoread.model.ReadMeta;
+
+public class BasicErrorPromptFormatter implements ErrorPromptFormatter {
+
+    private boolean showErrorType;
+
+    public BasicErrorPromptFormatter() {
+    }
+
+    public BasicErrorPromptFormatter(boolean showErrorType) {
+        this.showErrorType = showErrorType;
+    }
+
+    @Override
+    public String format(ReadMeta meta, String[] value, AnoReadException e) {
+
+        String typeMessage = "";
+        if (showErrorType) {
+            if (e instanceof ExtractionException) {
+                typeMessage = "[EXTRACTION]";
+            } else if (e instanceof ConversionException) {
+                typeMessage = "[CONVERSION]";
+            } else if (e instanceof ValidationError) {
+                typeMessage = "[VALIDATION]";
+            }
+
+            typeMessage += " ";
+        }
+
+        return String.format("%s%s - %s", typeMessage, meta.getName(), e.getDisplayMessage());
+    }
+}
---
+++ src/test/java/me/ronygomes/anoread/formatter/BasicErrorPromptFormatterTest.java
@@ -0,0 +1,69 @@
+package me.ronygomes.anoread.formatter;
+
+import me.ronygomes.anoread.exception.AnoReadException;
+import me.ronygomes.anoread.exception.ConversionException;
+import me.ronygomes.anoread.exception.ExtractionException;
+import me.ronygomes.anoread.exception.ValidationError;
+import me.ronygomes.anoread.formatter.impl.BasicErrorPromptFormatter;
+import me.ronygomes.anoread.model.ReadMeta;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BasicErrorPromptFormatterTest {
+
+    private static final String ERROR_META_NAME = "emName";
+    private static final String ERROR_META_PROMPT = "emPrompt";
+    private static final String ERROR_META_HINT = "emHint";
+    private static final String ERROR_DISPLAY_MESSAGE = "Message";
+
+    private ReadMeta meta;
+
+    @BeforeEach
+    public void setup() {
+        this.meta = new ReadMeta(ERROR_META_NAME, ERROR_META_PROMPT, ERROR_META_HINT);
+    }
+
+    @Test
+    void testExtractionExceptionWithoutPrompt() {
+        AnoReadException e = new ExtractionException(ERROR_DISPLAY_MESSAGE);
+        ErrorPromptFormatter ef = new BasicErrorPromptFormatter();
+        assertEquals("emName - Message", ef.format(meta, null, e));
+    }
+
+    @Test
+    void testExtractionExceptionWithoutOutPrompt() {
+        AnoReadException e = new ExtractionException(ERROR_DISPLAY_MESSAGE);
+        ErrorPromptFormatter ef = new BasicErrorPromptFormatter(true);
+        assertEquals("[EXTRACTION] emName - Message", ef.format(meta, null, e));
+    }
+
+    @Test
+    void testConversionExceptionWithoutPrompt() {
+        AnoReadException e = new ConversionException(ERROR_DISPLAY_MESSAGE);
+        ErrorPromptFormatter ef = new BasicErrorPromptFormatter();
+        assertEquals("emName - Message", ef.format(meta, null, e));
+    }
+
+    @Test
+    void testConversionExceptionWithoutOutPrompt() {
+        AnoReadException e = new ConversionException(ERROR_DISPLAY_MESSAGE);
+        ErrorPromptFormatter ef = new BasicErrorPromptFormatter(true);
+        assertEquals("[CONVERSION] emName - Message", ef.format(meta, null, e));
+    }
+
+    @Test
+    void testValidationErrorWithoutPrompt() {
+        AnoReadException e = new ValidationError(ERROR_DISPLAY_MESSAGE);
+        ErrorPromptFormatter ef = new BasicErrorPromptFormatter();
+        assertEquals("emName - Message", ef.format(meta, null, e));
+    }
+
+    @Test
+    void testValidationErrorWithoutOutPrompt() {
+        AnoReadException e = new ValidationError(ERROR_DISPLAY_MESSAGE);
+        ErrorPromptFormatter ef = new BasicErrorPromptFormatter(true);
+        assertEquals("[VALIDATION] emName - Message", ef.format(meta, null, e));
+    }
+}
