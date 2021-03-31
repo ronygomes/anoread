5ce4fa18d4e4ceab0da5a4af50d31b45872ef9d9
Manuel Rony Gomes <rgomes.bd@gmail.com>
2021-03-31 13:31:38 +0600

    Remove target TYPE from ExtractSingleInput annotation

--- src/main/java/me/ronygomes/anoread/annotation/extractor/ExtractSingleInput.java
+++ src/main/java/me/ronygomes/anoread/annotation/extractor/ExtractSingleInput.java
@@ -8,7 +8,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.FIELD})
+@Target(ElementType.FIELD)
 @InputExtractorType(provider = SingleInputExtractorProvider.class)
 public @interface ExtractSingleInput {
 
