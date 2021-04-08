c2355effd5ba443e096a75e7e5df1132d3e81834
Manuel Rony Gomes <rgomes.bd@gmail.com>
2021-04-08 18:56:04 +0600

    Add unittest in EngineComponentProviderTest

--- src/test/java/me/ronygomes/anoread/processor/EngineComponentProviderTest.java
+++ src/test/java/me/ronygomes/anoread/processor/EngineComponentProviderTest.java
@@ -1,21 +1,72 @@
 package me.ronygomes.anoread.processor;
 
+import me.ronygomes.anoread.converter.InputConverter;
 import me.ronygomes.anoread.converter.impl.StringConverter;
+import me.ronygomes.anoread.extractor.InputExtractor;
 import me.ronygomes.anoread.extractor.impl.SingleInputExtractor;
+import me.ronygomes.anoread.formatter.ErrorPromptFormatter;
+import me.ronygomes.anoread.formatter.ReadPromptFormatter;
 import me.ronygomes.anoread.formatter.impl.BasicErrorPromptFormatter;
 import me.ronygomes.anoread.formatter.impl.BasicReadPromptFormatter;
+import me.ronygomes.anoread.handler.ReadHandler;
 import me.ronygomes.anoread.handler.impl.SingleLineReadHandler;
 import me.ronygomes.anoread.model.Person;
 import me.ronygomes.anoread.model.ReadTask;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.lang.reflect.Field;
+import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class EngineComponentProviderTest {
 
+    @Mock
+    private ReadHandler handler;
+
+    @Mock
+    private ReadPromptFormatter readPromptFormatter;
+
+    @Mock
+    private ErrorPromptFormatter errorPromptFormatter;
+
+    @Mock
+    private InputConverter<?> converter;
+
+    @Mock
+    private InputExtractor extractor;
+
+    @Mock
+    private Consumer<Object> assigner;
+
+    @Test
+    void testUpdateTaskUpdateReadTaskWithProviderData() throws NoSuchFieldException {
+        EngineComponentProvider p = spy(EngineComponentProvider.class);
+        ReadTask<?> task = mock(ReadTask.class);
+
+        when(p.getHandler()).thenReturn(handler);
+        when(p.getReadPromptFormatter()).thenReturn(readPromptFormatter);
+        when(p.getErrorPromptFormatter()).thenReturn(errorPromptFormatter);
+        doReturn(converter).when(p).getConverter(any());
+        when(p.getExtractor(any())).thenReturn(extractor);
+        when(p.getAssigner(any(), any())).thenReturn(assigner);
+
+        p.updateTask(null, Person.class.getDeclaredField("name"), task);
+
+        verify(task, times(1)).setHandler(handler);
+        verify(task, times(1)).setReadPromptFormatter(readPromptFormatter);
+        verify(task, times(1)).setErrorPromptFormatter(errorPromptFormatter);
+        verify(task, times(1)).setConverter(converter);
+        verify(task, times(1)).setExtractor(extractor);
+        verify(task, times(1)).setAssigner(assigner);
+
+    }
 
     @Test
     void testUpdateTaskCopiesCorrectly() throws NoSuchFieldException {
